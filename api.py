ncss/necsus

commit 313cb6e4a00edd655cfc277740e12f27922bd01e

client: purge the #general channel and add lobby
A default channel with no auth is a bad idea, not to mention that it is
slightly confusing how you join a room.

Signed-off-by: Aleksa Sarai <cyphar@cyphar.com>

api.py
@@ -271,9 +271,8 @@ def post_message():
   """
 
   data = request.get_json()
-  # room is allowed to be blank because "" is a room
-  if not data or not data.get('text') or data.get('room') is None or not data.get('author'):
-    return jsonify({'message': 'text, room, and author keys are required'}), 400
+  if not data or not data.get('text') or data.get('room') or not data.get('author'):
+    return jsonify({'message': 'text, room, and author keys must be present and non-empty'}), 400
 
   db = get_db()
 

client/index.html
@@ -165,9 +165,9 @@
                 <i class="material-icons">clear_all</i>&nbsp;
                 <span>Reset Room</span>
               </button>
-              <label for="confirm-reset-room-input" v-if="resetRoomShow"
-                >Enter room name ({{ room }}) to confirm deletion:</label
-              >
+              <label for="confirm-reset-room-input" v-if="resetRoomShow">
+                Enter room name ({{ room }}) to confirm deletion:
+              </label>
               <input
                 id="confirm-reset-room-input"
                 name="confirm-reset-room"

client/lobby.html
@@ -1,0 +1,70 @@
+<!DOCTYPE html>
+<html>
+  <head>
+    <meta charset="utf-8" />
+    <title>NeCSuS</title>
+    <script src="client/js/marked.min.js"></script>
+    <script src="client/js/marked-plaintext.js"></script>
+    <script src="client/js/vue.js"></script>
+    <link rel="stylesheet" type="text/css" href="client/necsus.css" />
+    <link
+      href="https://fonts.googleapis.com/icon?family=Material+Icons"
+      rel="stylesheet"
+    />
+    <link
+      rel="stylesheet"
+      href="https://cdnjs.cloudflare.com/ajax/libs/materialize/1.0.0/css/materialize.min.css"
+    />
+    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/materialize/1.0.0/js/materialize.min.js"></script>
+
+  </head>
+
+  <body>
+    <div id="necsus">
+      <div class="navbar-fixed">
+        <nav class="z-depth-0">
+          <div class="nav-wrapper">
+            <span class="nav-name">NeCSuS</span>
+          </div>
+        </nav>
+      </div>
+
+      <div id="lobby">
+        <div class="card">
+          <div class="card-content">
+            <span class="card-title">{{ welcome }}!</span>
+
+            <p>
+              Welcome to the NeCSuS lobby! Please enter the room you'd like to
+              join.
+            </p>
+
+            <label for="room-name">
+              <span>Room name</span>
+            </label>
+
+            <input
+              id="room-name"
+              name="room-name"
+              v-model="room_name"
+            />
+
+            <button
+              id="join-room"
+              class="btn btn-wide"
+              :disabled="room_name == ''"
+              @click="joinRoom()"
+            >
+              <i class="material-icons">flight_takeoff</i>&nbsp;
+              <span>Join room {{ room_name }}</span>
+            </button>
+          </div>
+        </div>
+      </div>
+    </div>
+
+    <script src="client/lobby.js"></script>
+  </body>
+</html>
+

client/lobby.js
@@ -1,0 +1,40 @@
+let app = new Vue({
+  el: '#necsus',
+  data: {
+    room_name: '',
+    welcome: undefined,
+    possibleWelcomes: [
+      "G'day",
+      "Welcome",
+      "Dobrodošli",
+      "Добродошли",
+      "Добро пожаловать",
+      "Bienvenue",
+      "Willkommen",
+      "ようこそ",
+      "欢迎",
+      "Selamat datang",
+      "Yarrr",
+      "Ahoy",
+      "Arrr me hearties, yo-ho",
+    ],
+  },
+  created: function() {
+    this.refreshWelcome();
+  },
+  methods: {
+    refreshWelcome: function() {
+      let vm = this;
+      vm.welcome = vm.randomChoice(vm.possibleWelcomes);
+      window.setTimeout(() => vm.refreshWelcome(), 5000)
+    },
+    randomChoice: function(array) {
+      return array[Math.floor(Math.random() * array.length)];
+    },
+    joinRoom: function() {
+      if (this.room_name !== "") {
+        window.location = window.location.origin + `/${this.room_name}`;
+      }
+    },
+  },
+});

client/necsus.css
@@ -276,3 +276,11 @@ tr.identical-bot {
 .flex-grow {
   flex-grow: 1;
 }
+
+#lobby {
+  /* Looks like centering doesn't wanna work, I guess it's not centered now. */
+  padding-top: 10vh;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+}

client/necsus.js
@@ -436,8 +436,6 @@ let app = new Vue({
         }
         return m;
       })
-
-
     }
   },
   watch: {

frontend.py
@@ -7,8 +7,11 @@ def client_path(path):
   return send_from_directory('client', path)
 
 @app.route('/')
+def lobby():
+  return send_from_directory('client', 'lobby.html')
+
 @app.route('/<room>')
-def client(room=''):
+def client(room):
   return send_from_directory('client', 'index.html')
 
 @app.route('/api', methods=['GET'])

commit 3a8114cf48a4591ab83e7a3e8648cacb0780ac1f

api: un-break message sending 😬
Fixes: 313cb6e4a00e ("client: purge the #general channel and add lobby")
Signed-off-by: Aleksa Sarai <cyphar@cyphar.com>

api.py
@@ -271,7 +271,9 @@ def post_message():
   """
 
   data = request.get_json()
-  if not data or not data.get('text') or data.get('room') or not data.get('author'):
+  if not data:
+    return jsonify({'message': 'message must be valid JSON object'}), 400
+  if not all([data.get('text'), data.get('room'), data.get('author')]):
     return jsonify({'message': 'text, room, and author keys must be present and non-empty'}), 400
 
   db = get_db()