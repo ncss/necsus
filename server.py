5834b86656d8af42b0e16303d3fd901ffa840bfb
Joel Gibson <joel@jgibson.id.au>
2023-01-03 20:54:47 +1100

    Updated dependencies.

--- server.py
+++ server.py
@@ -1,5 +1,9 @@
 from necsus import app, init_db
 
+# Importing these for effect (they add routes to the app)
+import api
+import frontend
+
 if __name__ == '__main__':
   init_db()
   app.run(host="0.0.0.0", port=6277)
