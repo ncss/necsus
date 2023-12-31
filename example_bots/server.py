ccbab32b7b5e8ca64fd6cd9c510dbe7f46f4c159
Joel Gibson <joel@jgibson.id.au>
2023-12-31 15:11:33 +1100

    Unit tests.

--- example_bots/server.py
+++ example_bots/server.py
@@ -1,7 +1,8 @@
 """
-Some example bots to play with.
-- Debug bot (/debugbot) will just pretty-print whatever JSON the server sent it back into the chatroom.
-- Botty Botsen (/botty) uses the conversation API.
+Test bots is a Flask server containing examples of simple bots.
+These are also used to run automated tests on the NeCSuS server.
+
+The bots below are in ascending order of complexity (ish).
 """
 
 import html
@@ -14,176 +15,201 @@ from flask import Flask, request
 app = Flask(__name__)
 
 
-@app.route('/debugbot', methods=['POST'])
-def index():
-  pretty = pprint.pformat(request.json, indent=2)
-  return {
-    'author': 'Debug bot',
-    'text': f'''
-      <pre>{html.escape(pretty)}</pre>
-      <p>Here's a button if you want to check that too: <form><button name="some-name" value="some-value">Click me!</button></form></p>
-      <p>The button should trigger me again.</p>
-    ''',
-  }
+@app.post('/hellobot')
+def hellobot():
+    """HelloBot responds with 'Hello, world!'"""
+
+    return {'author': 'HelloBot', 'text': 'Hello, world!'}
 
 
-@app.route('/animal', methods=['POST'])
+@app.post('/echobot')
+def echobot():
+    """EchoBot responds with the original message text."""
+
+    data = request.json
+    return {'author': 'EchoBot', 'text': data['text']}
+
+
+@app.post('/debugbot')
+def debugbot():
+    """
+    DebugBot responds with a <pre>-formatted, pretty-printed copy of the complete JSON it was sent to the bot.
+    It also includes a button so that the different JSON for a form activation can be seen.
+    """
+
+    pretty = pprint.pformat(request.json, indent=2)
+    return {
+        'author': 'DebugBot',
+        'text': f'''
+            <pre>{html.escape(pretty)}</pre>
+            <p>Here's a button if you want to check that too: <form><button name="some-name" value="some-value">Click me!</button></form></p>
+            <p>The button should trigger me again.</p>
+        ''',
+    }
+
+
+@app.post('/imagebot')
 def animal():
-  """Test the image responses."""
-  return {
-    'author': 'Animal bot',
-    'text': 'Here is a capybara',
-    'image': 'https://upload.wikimedia.org/wikipedia/commons/thumb/e/ec/Capybara_%28Hydrochoerus_hydrochaeris%29.JPG/440px-Capybara_%28Hydrochoerus_hydrochaeris%29.JPG',
-  }
-
-BOTTY = 'Botty Botsen'
+    """ImageBot responds with a picture of a 'randomly' chosen animal in the special 'image' field."""
+    return {
+        'author': 'ImageBot',
+        'text': 'Here is a capybara.',
+        'image': 'https://upload.wikimedia.org/wikipedia/commons/thumb/e/ec/Capybara_%28Hydrochoerus_hydrochaeris%29.JPG/440px-Capybara_%28Hydrochoerus_hydrochaeris%29.JPG',
+    }
 
 
-@app.route('/botty', methods=['POST'])
-def bot():
-  data = request.get_json()
-  pprint.pprint(data)
-  text = data['text']
+@app.post('/statebot')
+def statebot():
+    """
+    StateBot is an example of how state can be used. Once activated, it will respond with the conversation so far (in
+    bullet-point form), and ask for another line. It can be stopped by writing 'stop' somewhere in the line, which will
+    cause the state to be removed.
+    - 'stop' by itself will return a JSON object with a missing 'state' key, which should end the conversation.
+    - 'stop null' will return a JSON object with {state: null}, which should end the conversation.
+    - 'stop empty string' will return a JSON object with {state: ''}, which should *not* end the conversation.
+    """
+    name = 'StateBot'
 
-  if 'stop' in text.lower():
-    if 'null' in text.lower():
-      return {
-        'text': 'I am stopping the conversation by returning null in the state',
-        'state': None,
-        'author': BOTTY,
-      }
-    elif 'empty string' in text.lower():
-      return {
-        'text': 'I am trying to break stuff by returning the empty string as a state',
-        'state': '',
-        'author': BOTTY,
-      }
+    data = request.json
+    text = data['text']
+    conversation = [*data.get('state', []), text]
+
+    html_lines = [
+        f'<p>I am the {name}. Here is your conversation so far:</p>',
+        '<ol>',
+        *[f'<li>{line}</li>' for line in conversation],
+        '</ol>',
+    ]
+    response = {'author': name, 'state': conversation}
+
+    if 'stop' in text.lower() and 'null' in text.lower():
+        html_lines += ['<p>I am stopping the conversation by returning <code>null</code> in the state</p>']
+        response['state'] = None
+    elif 'stop' in text.lower() and 'empty string' in text.lower():
+        html_lines += ['<p>I am trying to break stuff by returning the empty string as a state</p>']
+        response['state'] = ''
+    elif 'stop' in text.lower():
+        html_lines += ['<p>I am stopping the conversation by not returning a <code>state</code> key in the JSON object.</p>']
+        del response['state']
     else:
-      return {
-        'text': 'I am stopping the conversation by not returning a state',
-        'author': BOTTY,
-      }
+        html_lines += ['<p>Please enter another line, or say "stop", "stop null", or "stop empty string" (or any line with those words in them).<p>']
 
-  messages = data.get('state', [])
-  messages.append(text)
-
-  return {
-    'text': 'I am continuing the conversation. Here is a list of your previous messages: ' + ', '.join(messages),
-    'author': 'Botty Botsen',
-    'state': messages,
-  }
+    response['text'] = '\n'.join(html_lines)
+    return response
 
 
-@app.route('/sleep', methods=['POST'])
-def sleep():
-  """Slow route for testing a bot taking a long time to respond."""
-  if m := re.search(r'\d+', request.json['text']):
-    sleep_time = int(m.group())
-  else:
-    sleep_time = 10
+@app.post('/sleepbot')
+def sleepbot():
+    """
+    Slow route for testing a bot taking a long time to respond.
+    Takes the first whole number found in the prompt and sleeps for that many seconds, otherwise sleeps for 10.
+    """
+    if m := re.search(r'\d+', request.json['text']):
+        sleep_time = int(m.group())
+    else:
+        sleep_time = 10
 
-  time.sleep(sleep_time)
-  return {
-    'author': "SleepBot",
-    'text': f"I've slept for {sleep_time} seconds."
-  }
+    time.sleep(sleep_time)
+    return {
+        'author': "SleepBot",
+        'text': f"I've slept for {sleep_time} seconds."
+    }
 
 
 @app.route('/form', methods=['POST'])
 def form():
-  """Example route for forms."""
-  return {
-    'author': 'FormBot',
-    'text': '''
-      <p>You have two options:</p>
-      <form method="POST" action="/form/handler">
-        <button name="mybutton" value="1">Option 1</button>, or
-        <button name="mybutton" value="2">Option 2</button>.
-      </form>
-      ''',
-  }
+    """Example route for forms."""
+    return {
+        'author': 'FormBot',
+        'text': '''
+            <p>You have two options:</p>
+            <form method="POST" action="/form/handler">
+                <button name="mybutton" value="1">Option 1</button>, or
+                <button name="mybutton" value="2">Option 2</button>.
+            </form>
+            ''',
+    }
+
 
 @app.route('/form/handler', methods=['POST'])
 def form_handler():
-  # Expect an object of the form {room: string, form_data: object}.
-  data = request.get_json()
-  chosen = data['form_data']['mybutton']
-  return {
-    'author': 'FormBot',
-    'text': f"Option {chosen} is an excellent choice.",
-  }
+    # Expect an object of the form {room: string, form_data: object}.
+    data = request.get_json()
+    chosen = data['form_data']['mybutton']
+    return {
+        'author': 'FormBot',
+        'text': f"Option {chosen} is an excellent choice.",
+    }
 
 
 @app.route('/numberwang', methods=['POST'])
 def numberwang():
-  def form_html(mid):
-    return f"""
-      <form>
-        <p>Is your number
-          <button name="guess" value="lower">Lower</button>,
-          <button name="guess" value="correct">{mid}</button>, or
-          <button name="guess" value="higher">Higher</button>?
-      </form>
-    """
+    def form_html(mid):
+        return f"""
+            <form>
+                <p>Is your number
+                    <button name="guess" value="lower">Lower</button>,
+                    <button name="guess" value="correct">{mid}</button>, or
+                    <button name="guess" value="higher">Higher</button>?
+            </form>
+        """
 
-  data = request.get_json()
-  if 'state' not in data:
-    lo, hi = 1, 100
+    data = request.get_json()
+    if 'state' not in data:
+        lo, hi = 1, 100
+        mid = (lo + hi) // 2
+        return {
+            'author': 'Numberwang',
+            'text': f"<p>Pick a number between 1 and 100 and I will guess it! My first guess is {mid}.</p>{form_html(mid)}",
+            'state': [lo, hi],
+        }
+
+    if 'form_data' in data:
+        guess = data['form_data']['guess']
+    else:
+        guess = 'lower' if 'lower' in data['text'].lower() else 'higher' if 'higher' in data[
+            'text'].lower() else 'correct'
+
+    if guess == 'correct':
+        return {'author': 'Numberwang', 'text': "Woohoo, I win! Let's rotate the board!"}
+
+    print(data)
+    lo, hi = data.get('state', [1, 100])
+    mid = (lo + hi) // 2
+    if guess == 'lower':
+        hi = mid - 1
+    else:
+        lo = mid + 1
+
     mid = (lo + hi) // 2
     return {
-      'author': 'Numberwang',
-      'text': f"<p>Pick a number between 1 and 100 and I will guess it! My first guess is {mid}.</p>{form_html(mid)}",
-      'state': [lo, hi],
+        'author': 'numberwang',
+        'text': form_html(mid),
+        'state': [lo, hi],
     }
-
-  if 'form_data' in data:
-    guess = data['form_data']['guess']
-  else:
-    guess = 'lower' if 'lower' in data['text'].lower() else 'higher' if 'higher' in data['text'].lower() else 'correct'
-
-  if guess == 'correct':
-    return {'author': 'Numberwang', 'text': "Woohoo, I win! Let's rotate the board!"}
-
-  print(data)
-  lo, hi = data.get('state', [1, 100])
-  mid = (lo + hi) // 2
-  if guess == 'lower':
-    hi = mid - 1
-  else:
-    lo = mid + 1
-
-  mid = (lo + hi) // 2
-  return {
-    'author': 'numberwang',
-    'text': form_html(mid),
-    'state': [lo, hi],
-  }
 
 
 @app.route('/ping', methods=['POST'])
 def ping():
-  """
-  The ping bot should post a button, and upon clicking that, we should be in a conversation with the pong bot.
-  If we do "ping nowhere", then it will try to ping a bot that is not in the room.
-  """
-  action = '/nowhere' if 'nowhere' in request.json['text'].lower() else '/pong'
-  return {
-    'author': 'PingBot',
-    'text': f'<form action="{action}"><button>Go to {action}</button</form>',
-  }
+    """
+    The ping bot should post a button, and upon clicking that, we should be in a conversation with the pong bot.
+    If we do "ping nowhere", then it will try to ping a bot that is not in the room.
+    """
+    action = '/nowhere' if 'nowhere' in request.json['text'].lower() else '/pong'
+    return {
+        'author': 'PingBot',
+        'text': f'<form action="{action}"><button>Go to {action}</button</form>',
+    }
+
 
 @app.route('/pong', methods=['POST'])
 def pong():
-  data = request.json
+    data = request.json
 
-  if 'state' in data:
-    return {'author': 'PongBot', 'text': 'Thanks for chatting. This is PongBot signing off.'}
+    if 'state' in data:
+        return {'author': 'PongBot', 'text': 'Thanks for chatting. This is PongBot signing off.'}
 
-  if 'form_data' not in data:
-    return {'author': 'PongBot', 'text': 'Please activate PongBot via the PingBot.'}
+    if 'form_data' not in data:
+        return {'author': 'PongBot', 'text': 'Please activate PongBot via the PingBot.'}
 
-  return {'author': 'PongBot', 'text': 'This is PongBot. How are you today?', 'state': 0}
-
-
-if __name__ == '__main__':
-  app.run(port=1234, debug=True)
+    return {'author': 'PongBot', 'text': 'This is PongBot. How are you today?', 'state': 0}
---
+++ tests/test_necsus.py
@@ -0,0 +1,199 @@
+import json
+import pathlib
+import subprocess
+
+import asgiref.wsgi
+import httpx
+import pytest
+import respx
+from starlette.testclient import TestClient
+
+from example_bots import app as example_bots_app
+from necsus import app as necsus_app
+from necsus.server import create_db_connection
+
+# The necsus() fixture resets the database for each test, so this room should always start empty.
+TEST_ROOM = 'test_room'
+TEST_AUTHOR = 'TestAuthor'
+EXAMPLE_BOTS_URL = 'http://localhost:12345'
+
+
+@pytest.fixture
+def necsus() -> TestClient:
+    """
+    Return a TestClient (fake web server for testing) for Necsus.
+    We have set NECSUS_DB=:memory: in conftest.py, so this fixture returns a clean database in each test.
+    """
+    with TestClient(necsus_app) as necsus:
+        yield necsus
+
+
+@pytest.fixture
+def example_bots() -> respx.MockRouter:
+    with respx.mock(base_url=EXAMPLE_BOTS_URL) as respx_mock:
+        # Joel: One would think we could just use respx.WSGIHandler(flask_app) rather than doing this conversion.
+        #       It doesn't work: some difference deep in httpx or respx, to do with WSGITransport and an AsyncClient.
+        example_bots_asgi_app = asgiref.wsgi.WsgiToAsgi(example_bots_app)
+        yield respx_mock.route().mock(side_effect=respx.ASGIHandler(example_bots_asgi_app))
+
+
+def test_necsus_fixture(necsus: TestClient):
+    """Test that the TestClient fixture for the NeCSuS server is working."""
+    response = necsus.get('/')
+    assert response.status_code == 200
+
+
+@pytest.mark.parametrize('anyio_backend', [('asyncio', {'use_uvloop': True})])
+async def test_example_bots_fixture(anyio_backend, example_bots: respx.MockRouter):
+    """Test that the MockRouter fixture for example bots is working."""
+    async with httpx.AsyncClient() as client:
+        response = await client.post(f'{EXAMPLE_BOTS_URL}/hellobot', json={})
+
+    assert response.status_code == 200
+    assert response.json()['text'] == 'Hello, world!'
+
+
+def test_post_clear_messages(necsus: TestClient):
+    """
+    Test that we can post messages into a room, retrieve them, and clear the room. This test exercises:
+    - GET /api/messages
+    - POST /api/actions/message
+    - POST /api/actions/clear-room-messages
+    """
+
+    # Ensure the room is empty to start with.
+    assert necsus.get('/api/messages', params={'room': TEST_ROOM}).json() == []
+
+    # Post a message, ensuring the returned message from the server has the same fields (and more) as our message.
+    user_message = {'room': TEST_ROOM, 'author': TEST_AUTHOR, 'text': 'Hello there.'}
+    server_message = necsus.post('/api/actions/message', json=user_message).json()
+    assert user_message.items() <= server_message.items()
+
+    # Ensure the message is in the room.
+    assert necsus.get('/api/messages', params={'room': TEST_ROOM}).json() == [server_message]
+
+    # Clear the room, and ensure there is nothing remaining.
+    assert necsus.post('/api/actions/clear-room-messages', json={'room': TEST_ROOM}).status_code == 200
+    assert necsus.get('/api/messages', params={'room': TEST_ROOM}).json() == []
+
+
+def test_add_remove_bots(necsus: TestClient):
+    """
+    Test that we can add a bot to a room and remove it again. This test exercises:
+    - GET /api/bots
+    - POST, DELETE /api/actions/bot
+    """
+
+    # Ensure there are no bots in the room.
+    assert necsus.get('/api/bots', params={'room': TEST_ROOM}).json() == []
+
+    # Add a bot, ensuring the returned bot has the same fields (and more) as our bot.
+    user_bot = {'room': TEST_ROOM, 'name': 'TestBot', 'url': 'http://definitely/a/url'}
+    server_bot = necsus.post('/api/actions/bot', json=user_bot).json()
+    assert user_bot.items() <= server_bot.items()
+
+    # Ensure the bot is in the room.
+    assert necsus.get('/api/bots', params={'room': TEST_ROOM}).json() == [server_bot]
+
+    # Remove the bot from the room, and ensure there are no bots remaining.
+    assert necsus.request('delete', '/api/actions/bot', json={'id': server_bot['id']}).status_code == 200
+    assert necsus.get('/api/bots', params={'room': TEST_ROOM}).json() == []
+
+
+def test_echo(necsus: TestClient, example_bots: respx.MockRouter):
+    """
+    Test a full round-trip on EchoBot, which checks:
+
+    - Asserting the room has no bots to begin with.
+    - Installing EchoBot into a room (with no responds-to, so it should only respond to its name).
+    - Verifying EchoBot is in the room.
+    - Posting a message which should not trigger the bot.
+    - Posting a message which should trigger the bot.
+    - Verifying the JSON sent to and from the bot.
+    - Verifying the final message posted back to the room.
+    """
+
+    # Assert the room is empty to begin with (of both messages and bots).
+    assert necsus.get('/api/messages', params={'room': TEST_ROOM}).json() == []
+    assert necsus.get('/api/bots', params={'room': TEST_ROOM}).json() == []
+
+    # Install the bot.
+    response = necsus.post('/api/actions/bot', json={
+            'room': TEST_ROOM,
+            'name': 'EchoBot',
+            'url': f'{EXAMPLE_BOTS_URL}/echobot',
+        },
+    )
+    assert response.status_code == 200
+    bot_id = response.json()['id']
+
+    # Check the bot shows up in the API.
+    assert necsus.get('/api/bots', params={'room': TEST_ROOM}).json() == [
+        {
+            'id': bot_id,
+            'room': TEST_ROOM,
+            'name': 'EchoBot',
+            'responds_to': None,
+            'url': f'{EXAMPLE_BOTS_URL}/echobot'
+        },
+    ]
+
+    # Post a message which should not trigger the bot.
+    necsus.post('/api/actions/message', json={'room': TEST_ROOM, 'author': 'Joel', 'text': 'Hello there.'})
+    assert len(example_bots.calls) == 0
+
+    # Trigger a message to the bot.
+    user_message = {'room': TEST_ROOM, 'author': 'Joel', 'text': 'Hello there, echobot.'}
+    response = necsus.post('/api/actions/message', json=user_message)
+
+    # Check that the bot got a message of the right shape.
+    bot_request = example_bots.calls.last.request
+    assert bot_request.method == 'POST'
+    bot_json = json.loads(bot_request.content)
+    assert bot_json == {
+        'room': TEST_ROOM,
+        'author': 'Joel',
+        'text': 'Hello there, echobot.',
+        'params': {},
+    }
+
+    # Check that the bot responded correctly.
+    bot_response = example_bots.calls.last.response
+    assert bot_response.status_code == 200
+
+    # Check that our final message post returned successfully.
+    assert response.status_code == 200
+
+    # Message subset that we expect in reply.
+    expected_reply = {
+        'room': TEST_ROOM,
+        'author': 'EchoBot',
+        'kind': 'bot',
+        'text': 'Hello there, echobot.',
+        'image': None,
+        'media': None,
+        'from_bot': bot_id,
+        'state': None,
+    }
+
+    # Check that the message made it back to the room.
+    messages = necsus.get('/api/messages', params={'room': TEST_ROOM}).json()
+    assert len(messages) == 3
+    assert user_message.items() <= messages[-2].items()
+    assert expected_reply.items() <= messages[-1].items()
+
+
+def test_schema(tmp_path: pathlib.Path):
+    """
+    The database Schema had a strange issue with double-quote and single-quote confusion, which led to internal errors
+    in Sqlite3 when trying to take backups.
+    """
+
+    # Create an empty database on disk, initialised with the NeCSuS schema.
+    db_path = str(tmp_path / 'necsus.db')
+    create_db_connection(db_path).close()
+
+    # Attempt to run a backup of it using the Sqlite3 CLI (for some reason this error does not happen in Python).
+    backup_path = str(tmp_path / 'necsus.backup.db')
+    backup_result = subprocess.run(['sqlite3', db_path, f"""VACUUM INTO '{backup_path}'"""])
+    assert backup_result.returncode == 0
